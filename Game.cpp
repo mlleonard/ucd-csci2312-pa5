76446158e3196ff6d0ed2012cf827e0ff88d05a0
madelineleonard <mlleonard3331@gmail.com>
2015-12-15 12:51:33 -0700

    Piece and Game cpp

--- Game.cpp
+++ Game.cpp
@@ -11,6 +11,12 @@
 
 namespace Gaming{
 
+    const unsigned int NUM_INIT_AGENT_FACTOR = 4;
+    const unsigned int NUM_INIT_RESOURCE_FACTOR = 2;
+    const unsigned MIN_WIDTH = 3;
+    const unsigned MIN_HEIGHTH = 3;
+    const double STARTING_AGENT_ENERGY = 20;
+    const double STARTING_RESOURCE_CAPACITY = 10;
 
     void Game::populate()
     {
