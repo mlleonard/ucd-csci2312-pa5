368d05a15a755cff399b2ba38f6f19e88230636c
madelineleonard <mlleonard3331@gmail.com>
2015-12-15 15:13:07 -0700

    Piece and Game cpp

--- Agent.cpp
+++ Agent.cpp
@@ -2,3 +2,31 @@
 // Created by Madeline Leonard on 12/14/15.
 //
 
+#include "Agent.h"
+
+namespace Gaming{
+
+
+    Agent::Agent(const Game &g, const Position &p, double energy) : Piece(g,p)
+    {
+        __energy = energy;
+    }
+
+
+    Agent::~Agent()
+    {
+        std::cout << "Calling derived destructor" << std::endl;
+    }
+
+    void Agent::age()
+    {
+        double age;
+
+        age = (__energy - AGENT_FATIGUE_RATE);
+
+        if (age == 0)
+        {
+            finish();
+        }
+    }
+};
